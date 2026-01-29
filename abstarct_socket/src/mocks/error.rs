e84f0d0dce025ef8cd893632ed2d87c5cd18f913
Dmitriy Krasnyansky <k.dimonych@gmail.com>
2026-01-29 15:24:42 +0200

    refactor: DummyReadError and DummyWriteError are alias of DummyStreamError

--- abstarct_socket/src/mocks/error.rs
+++ abstarct_socket/src/mocks/error.rs
@@ -1,19 +1,24 @@
-/// Error returned by TcpSocket read/write functions.
+/// Error returned by stream read functions.
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum DummyReadError {
+pub enum DummyStreamError {
     /// The connection was reset.
     ///
     /// This can happen on receiving a RST packet, or on timeout.
     ConnectionReset,
 }
 
+/// Alias for write errors returned by stream write functions.
+pub type DummyReadError = DummyStreamError;
+/// Alias for write errors returned by stream write functions.
+pub type DummyWriteError = DummyStreamError;
+
 mod embedded_io_impls {
     use super::*;
 
-    impl embedded_io_async::Error for DummyReadError {
+    impl embedded_io_async::Error for DummyStreamError {
         fn kind(&self) -> embedded_io_async::ErrorKind {
             match self {
-                DummyReadError::ConnectionReset => embedded_io_async::ErrorKind::ConnectionReset,
+                DummyStreamError::ConnectionReset => embedded_io_async::ErrorKind::ConnectionReset,
             }
         }
     }
--- protocols/src/mocks/error.rs
+++ protocols/src/mocks/error.rs
@@ -1,10 +1,10 @@
 use crate::error::Error;
-use abstarct_socket::mocks::error::DummyReadError;
+use abstarct_socket::mocks::error::DummyStreamError;
 
-impl From<DummyReadError> for Error {
-    fn from(err: DummyReadError) -> Self {
+impl From<DummyStreamError> for Error {
+    fn from(err: DummyStreamError) -> Self {
         match err {
-            DummyReadError::ConnectionReset => {
+            DummyStreamError::ConnectionReset => {
                 Error::SocketError(embassy_net::tcp::Error::ConnectionReset)
             }
         }
